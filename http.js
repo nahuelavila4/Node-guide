360d15836de88da5f3709c0e55e89b6f63328b5f
Nahuel Avila <avilanahuel99@gmail.com>
2022-12-04 02:17:14 -0300

    npm

---
+++ http.js
@@ -0,0 +1,29 @@
+const http = require("http");
+
+// listen dice en que puerto va a estar funcionando el server
+
+http.createServer(function (request, response){
+    console.log(request.url)
+
+    if(request.url === "/"){
+        response.write("Welcome to the server")
+        return response.end()
+    }
+
+    if(request.url === "/about"){
+        response.write("acerca de")
+        return response.end()
+    }
+
+    response.write(`<h1>Not found</h1>
+    <p>Esta pagina no se encontro</p>
+    <a href="/">volver a la pagina principal</a>`);
+    response.end();
+}).listen(3000);
+
+console.log("Server creado en puerto 3000")
+
+
+
+
+
--- nf.js
+++ nf.js
@@ -22,9 +22,33 @@ fs.writeFileSync("./data/cuatro.txt", titulo, {
 // los metodos asincrtonos se pueden ejecutar al mismo
 // tiempo que otro
 
+// Desde function en adelante es un callback 
+// Ayuda a ejecutar logica adicional
+
 fs.readFile("./data/uno.txt", function (error, data) {
   if (error) {
     console.log(error);
   }
   console.log(data.toString());
+  fs.writeFile("./data/ejemplo-escritura.txt", "archivo escrito con fs", (err, data) => {
+    console.log(err);
+    console.log(data);
+  })
 });
+
+// Para leer varios archivos meter un readfile dentro de otro
+
+fs.readFile("./data/dos.txt", function (error, data) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(data.toString());
+  fs.readFile("./data/tres.txt", function (error, data) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(data.toString());
+  });
+  
+});
+
